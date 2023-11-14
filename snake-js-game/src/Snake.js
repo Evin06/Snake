66c8e57fd59dfd62c24519f3cc99148a6df48ee9
Evin06 <evinsasi@hotmail.com>
2023-11-14 15:21:28 +0100

    Affichage Snake et déplacement
    affichage d'une case et mise en place pour le déplacement du carré

--- snake-js-game/src/Snake.js
+++ snake-js-game/src/Snake.js
@@ -1,3 +1,23 @@
-class SNAKE{
+export class SNAKE {
 
+    constructor() {
+        const canvas = document.querySelector('canvas');
+        const ctx = canvas.getContext('2d');
+        this.x = 0;
+        this.y = 0;
+        this.size = 60;
+        console.log('Hello Snake 1');
+    }
+    moveLeft(){
+        this.x--
+    }
+    moveRight(){
+        this.x++
+    }
+    moveDown(){
+        this.y++
+    } 
+    moveUp(){
+        this.y--
+    }
 }
--- snake-js-game/src/main.js
+++ snake-js-game/src/main.js
@@ -1,22 +1,26 @@
 import '../css/style.css';
-import './Snake.js';
+import { SNAKE } from './Snake.js';
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
+
+const snake = new SNAKE
 
 const move = () => {
 
   // Dessine la grille de jeu
   ctx.fillStyle = 'black';
-  ctx.fillRect(0, 0, 800, 800);
-  
-  ctx.fillStyle = 'Green';
-  ctx.fillRect(0, 0, 60, 60);
+  ctx.fillRect(0, 0, 800, 800); 
+
+
+  ctx.fillStyle = 'red';
+  ctx.fillRect(snake.x * snake.size, snake.y * snake.size, snake.size, snake.size);
+
+};
+
 
   // Rafraichit à chaque seconde
   setTimeout(() => {
     requestAnimationFrame(move);
   }, 1000);
   
-};
 
-requestAnimationFrame(move);
