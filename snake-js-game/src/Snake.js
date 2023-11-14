2a0943ad2cdc4125dd79c256fc9bf9945fbd6835
Evin06 <evinsasi@hotmail.com>
2023-11-14 16:24:05 +0100

    Modification taille + positions du snake

--- snake-js-game/src/Snake.js
+++ snake-js-game/src/Snake.js
@@ -3,9 +3,9 @@ export class SNAKE {
     constructor() {
         const canvas = document.querySelector('canvas');
         const ctx = canvas.getContext('2d');
-        this.x = 6;
-        this.y = 6;
-        this.size = 60;
+        this.x = 10;
+        this.y = 10;
+        this.size = 40;
         console.log('Hello Snake 1');
     }
     moveLeft(){
